f813ec0ee9760feb59eb2fd514d312a922ab27d3
Vanisri <vanisri@pdx.edu>
2026-04-26 14:48:05 -0700

    Fix mac_correct.v: remove blocking assignment from always_ff

--- codefest/cf04/hdl/mac_correct.v
+++ codefest/cf04/hdl/mac_correct.v
@@ -6,22 +6,11 @@ module mac (
     output logic signed [31:0]  out
 );
 
-    logic signed [15:0] product;
-
     always_ff @(posedge clk) begin
         if (rst)
             out <= 32'sd0;
-        else begin
-            product = a * b;
-            out <= out + 32'(signed'(product));
-        end
+        else
+            out <= out + 32'(signed'(a * b));
     end
 
 endmodule
-
-
-
-
-
-
-
